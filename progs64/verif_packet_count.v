992f2a9a50fd9821662b32887c1cfc14023e13cd
swarnpriya <priya13swarn@gmail.com>
2024-08-28 15:13:18 -0400

    wip for packet_count example

--- progs64/verif_packet_count.v
+++ progs64/verif_packet_count.v
@@ -9,7 +9,6 @@ Local Open Scope logic.
 (* An abbreviation for C language type struct xdp_md *)
 Definition txdpmd := Tstruct _xdp_md noattr. 
  
-
 (* isptr p means p is not a null pointer, not NULL or Vundef or a floating point number *)
 (* data_at assertion says that at addresst p in memory there is a data structure of type 
    t_xdp_md with access-permission (Tsh: top permission) and content v*) 
@@ -19,17 +18,17 @@ Lemma data_at_isptr_xdp: forall v p,
 Proof.
 intros. entailer!.
 Qed.
-
+Search Int.add.
 Definition packet_count_spec : ident * funspec :=
 DECLARE _packet_count 
- WITH v : reptype' txdpmd, p : val, sh : share
+ WITH v : reptype' txdpmd, p : val, sh : share, gv: globals, c : int
  PRE [ tptr txdpmd ]
-  PROP ()
-  PARAMS (p) 
-  SEP (data_at sh txdpmd (repinj _ v) p)
+  PROP (readable_share sh)
+  PARAMS (p) GLOBALS (gv) 
+  SEP (data_at sh txdpmd (repinj _ v) p; data_at Ews tint (Vint c) (gv _counter))
  POST [ tint ]
   PROP () RETURN (Vint (Int.repr 2))
-  SEP (data_at Ews txdpmd (repinj _ v) p).
+  SEP (data_at sh txdpmd (repinj _ v) p; data_at Ews tint (Vint (Int.repr (Int.signed c + Int.signed (Int.repr 1)))) (gv _counter)).
 
 (*
 (* API spec for the packet_count.c program *)
@@ -65,5 +64,10 @@ Definition Gprog : funspecs := ltac:(with_library prog [packet_count_spec ]).
 Lemma body_packet_count : semax_body Vprog Gprog f_packet_count packet_count_spec.
 Proof.
 start_function.
+forward.
+forward.
++ entailer!. admit.
++ forward. entailer!. admit.
 Admitted.
+
 
